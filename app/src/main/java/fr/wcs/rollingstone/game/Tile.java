63c5afdce2d31ed24f4b33f1dfc3f3b87643aecc
giallon <nagiallombardo@gmail.com>
2018-12-11 10:52:04 +0100

    gitignore is now working

--- app/src/main/java/fr/wcs/rollingstone/game/Tile.java
+++ app/src/main/java/fr/wcs/rollingstone/game/Tile.java
@@ -2,12 +2,34 @@ package fr.wcs.rollingstone.game;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 public class Tile implements Sprite {
     private Bitmap bitmap;
+    private GameView gameView;
+
+    private int row;
+    private int column;
+
+    private int width;
+    private int heigth;
+
+    public Tile(Bitmap bitmap, GameView gameView) {
+        this.bitmap = bitmap;
+        this.gameView = gameView;
+        this.row = 0;
+        this.column = 0;
+        this.width = bitmap.getWidth() / this.row;
+        this.heigth = bitmap.getHeight() / this.column;
+    }
 
     @Override
     public void onDraw(Canvas canvas) {
         // draw Bitmap
+        int srcX = 0;
+        int srcY = 1 * this.heigth;
+        Rect src = new Rect(srcX, srcY, srcX + this.width, srcY + this.heigth);
+        Rect dest = new Rect(0, 0, this.width, this.heigth);
+        canvas.drawBitmap(this.bitmap, src, dest, null);
     }
 }
